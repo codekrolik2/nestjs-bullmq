b8824e1305fe3bb6cb91d71ea1feb5c1b2cea83b
codekrolik2 <john.amirov.83@gmail.com>
2025-06-17 13:10:12 -0800

    Serialized execution

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TRANSCODE_QUEUE } from './constants';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post(TRANSCODE_QUEUE)
+  async transcode() {
+    this.logger.log(`Transcode POST request received`);
+    return this.appService.transcode()
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
+import { TRANSCODE_QUEUE } from './constants';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger = new Logger(AppService.name);
+  constructor(@InjectQueue(TRANSCODE_QUEUE) private readonly transcodeQueue: Queue) {}
+
+  async transcode() {
+    this.logger.log(`Add to Transcode Queue`);
+    await this.transcodeQueue.add({
+      fileName: './file.mp3',
+    })
   }
 }
